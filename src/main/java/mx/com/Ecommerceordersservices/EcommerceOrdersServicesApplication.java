9f39c9699eaa92ffc35cd92694856a8570e6dba1
MoniFrias <friasmoni18@gmail.com>
2023-05-17 12:23:44 -0600

    Create orders-services

---
+++ src/main/java/mx/com/Ecommerceordersservices/EcommerceOrdersServicesApplication.java
@@ -0,0 +1,13 @@
+package mx.com.Ecommerceordersservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOrdersServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOrdersServicesApplication.class, args);
+	}
+
+}
